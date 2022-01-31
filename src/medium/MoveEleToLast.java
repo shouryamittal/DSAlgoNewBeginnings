40875dbbda71cab3a43a33374f292b8e3aeca95e
Shourya Mittal <mittalshourya97@gmail.com>
2022-01-31 20:58:59 +0530

    Smallest Difference & MoveEleToL

---
+++ src/medium/MoveEleToLast.java
@@ -0,0 +1,33 @@
+package medium;
+
+/**
+ * Q: Shift every occurrence of element X to the last of the array in place.
+ */
+public class MoveEleToLast {
+
+    //method: Use the same 2 pointer approach, point i to first and j to last.
+    private int[] moveEleToLast(int[] arr, int toMove) {
+        int i = 0, j = arr.length - 1;
+        while(i < j) {
+            while((i < j && arr[j] == toMove)) {
+                j--;
+            }
+            if(arr[i] == toMove) {
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+            i++;
+        }
+        return arr;
+    }
+    public static void main(String[] args) {
+        MoveEleToLast m = new MoveEleToLast();
+        int arr[] = {2,3,3,2,3,2,4,2};
+        int toMove = 3;
+        int result[] = m.moveEleToLast(arr, toMove);
+        for(int ele: result) {
+            System.out.println(ele);
+        }
+    }
+}
--- src/medium/SmallestDiff.java
+++ src/medium/SmallestDiff.java
@@ -1,9 +1,23 @@
 package medium;
-
 import java.util.Arrays;
+
+/**
+ * Q: Given two arrays of different sizes, find 2 numbers for which the difference is minimum.
+ */
 
 public class SmallestDiff {
 
+    //method 1: Sort both the arrays, then use the property of sorted arrays which we used in 2 and 3 number sum questions.
+    // put i at first element of arr1, and j at arr2
+    // if(arr1[i] == arr2[j]) -> means its the minimum difference, hence return with those numbers
+    // if(arr1[i] < arr2[j]) -> means, the difference between all the numbers after arr1[i] and the current value of arr[j] would be
+    // smaller than difference of arr1[i] and arr2[j]. hence i++
+    //if(arr1[i] > arr2[j] -> means ,the difference between all the numbers after arr2[j] and the current value of arr[i] would be
+    //smaller than difference of arr1[i] and arr2[j]. hence j++
+
+
+    //Time: O(NlogN + MlogM) -> N - size of arr1, M - size of arr2
+    //Space: O(1) - coz no extra space used.
     private int[] smallestDiff(int arr1[], int arr2[]) {
         Arrays.sort(arr1);
         Arrays.sort(arr2);
